78a2750d4dedf2401e5455c2afea1530d3d292f0
AnthonyPCPortable <anth06ny@gmail.com>
2025-11-03 11:28:40 +0100

    Correction UI complet

--- app/src/main/java/com/example/a25_10_ynov_android/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/example/a25_10_ynov_android/ui/screens/SearchScreen.kt
@@ -1,22 +1,33 @@
 package com.example.a25_10_ynov_android.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,7 +41,7 @@ import com.example.a25_10_ynov_android.viewmodel.MainViewModel
 @Preview(showBackground = true, showSystemUi = true)
 @Preview(
     showBackground = true, showSystemUi = true,
-    uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES or android.content.res.Configuration.UI_MODE_TYPE_NORMAL
+    uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES or android.content.res.Configuration.UI_MODE_TYPE_NORMAL, locale = "fr"
 )
 @Composable
 fun SearchScreenPreview() {
@@ -48,14 +59,81 @@ fun SearchScreenPreview() {
 fun SearchScreen(modifier: Modifier = Modifier, mainViewModel: MainViewModel = MainViewModel()) {
     Column(
         modifier = modifier
-            .fillMaxSize()
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        SearchBar()
+
         val list = mainViewModel.dataList.collectAsStateWithLifecycle().value
-        repeat(list.size) {
-            PictureRowItem(data = list[it])
-            Spacer(Modifier.height(10.dp))
+        //Permet de remplacer très facilement le RecyclerView. LazyRow existe aussi
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.weight(1f)
+            ) {
+            items(list.size) {
+                PictureRowItem(data = list[it])
+            }
+        }
+
+        Row {
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.Filled.Favorite,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text(stringResource(R.string.clear_filter))
+            }
+
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.Filled.Favorite,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text( stringResource(R.string.bt_load_data))
+            }
         }
     }
+}
+
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "", //Valeur affichée
+        onValueChange = {newValue:String -> }, //Nouveau texte entrée
+        leadingIcon = { //Image d'icône
+            Icon(
+                imageVector = Icons.Default.Search,
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = null
+            )
+        },
+        singleLine = true,
+        label = { //Texte d'aide qui se déplace
+            Text("Enter text")
+            //Pour aller le chercher dans string.xml, R de votre package com.nom.projet
+            //Text(stringResource(R.string.placeholder_search))
+        },
+        //placeholder = { //Texte d'aide qui disparait
+        //Text("Recherche")
+        //},
+
+        //keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search), // Définir le bouton "Entrée" comme action de recherche
+        //keyboardActions = KeyboardActions(onSearch = {onSearchAction()}), // Déclenche l'action définie
+        //Comment le composant doit se placer
+        modifier = modifier
+            .fillMaxWidth() // Prend toute la largeur
+            .heightIn(min = 56.dp) //Hauteur minimum
+    )
 }
 
 @Composable //Composable affichant 1 élément
